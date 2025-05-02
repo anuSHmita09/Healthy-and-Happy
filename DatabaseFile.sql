147f337ad73d1c9a6694f2a7ca89ab057f934086
Anushmita Pal <164021800+anuSHmita09@users.noreply.github.com>
2025-05-02 19:34:39 +0530

    Add files via upload

---
+++ DatabaseFile.sql
@@ -0,0 +1,8 @@
+SHOW DATABASES;
+USE HANDH;
+SHOW TABLES;
+DESC PERSONAL;
+DESC BODYEVAL;
+DESC PAYMENt;
+select * from bodyeval;
+insert into bodyeval values('1001','anushmita','','','','','','','','','','','2-5-25');
